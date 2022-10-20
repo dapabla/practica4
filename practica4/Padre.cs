ef8e8cd0738a8859690930493e88dffcc56b5f68
dapabla <dapabla@CCTL130-20>
2022-10-20 14:00:30 +0200

    cambios

--- practica4/Padre.cs
+++ practica4/Padre.cs
@@ -12,16 +12,20 @@ namespace practica4
 {
     public partial class Padre : Form
     {
+        int i = 0;
         public Padre()
         {
             InitializeComponent();
+            
         }
 
         private void a1_sub1_Click(object sender, EventArgs e)
         {
+            this.i = this.i + 1;
             Hijo hijo = new Hijo();
+            hijo.Text = "Documento " + i;
             hijo.MdiParent = this;
-            hijo.Text = "";
+      
             hijo.Show();
         }
 
