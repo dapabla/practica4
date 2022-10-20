db6fdd3c70d915abc27a6b7c2cafd0dcd1e099be
dapabla <dapabla@CCTL130-20>
2022-10-20 12:58:01 +0200

    cambios

--- practica4/Form1.Designer.cs
+++ practica4/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace practica4
 {
-    partial class Form1
+    partial class fPadre
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // fPadre
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "fPadre";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- practica4/Form1.cs
+++ practica4/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace practica4
 {
-    public partial class Form1 : Form
+    public partial class fPadre : Form
     {
-        public Form1()
+        public fPadre()
         {
             InitializeComponent();
         }
